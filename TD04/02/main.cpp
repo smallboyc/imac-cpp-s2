5eafb23d1570dcc222d1aafa150d50ef7ab096bc
smallboyc <maxencedup44@gmail.com>
2024-04-08 12:10:29 +0200

    fix : TD04 split_string

--- TD04/02/main.cpp
+++ TD04/02/main.cpp
@@ -14,27 +14,49 @@ int firstWordSize(std::string const &s)
     return std::distance(s.begin(), it);
 }
 
+//Méthode 2 pour split_string
 std::vector<std::string> split_string(std::string const &s)
 {
     std::vector<std::string> result;
-    std::string word;
-
-    for (auto it{s.begin()}; it != s.end(); it++)
+    auto it_start{s.begin()};
+    while (it_start != s.end())
     {
-        // à chaque ' ' on ajoute le mot au vector final
-        if (*it == ' ')
-        {
-            result.push_back(word);
-            word.clear();
-        }
-        // construction du mot
-        else
+        std::string word;
+        auto it_space{std::find_if(it_start, s.end(), is_space)};
+        int word_size = std::distance(it_start, it_space);
+        for (auto it = it_start; it != it_space; it++)
             word += *it;
+        result.push_back(word);
+        if (it_space != s.end())
+            it_start += word_size + 1;
+        else
+            break;
     }
-    // ajout du dernier mot
-    result.push_back(word);
     return result;
 }
+
+//Méthode 1 pour split_string
+// std::vector<std::string> split_string(std::string const &s)
+// {
+//     std::vector<std::string> result;
+//     std::string word;
+
+//     for (auto it{s.begin()}; it != s.end(); it++)
+//     {
+//         // à chaque ' ' on ajoute le mot au vector final
+//         if (*it == ' ')
+//         {
+//             result.push_back(word);
+//             word.clear();
+//         }
+//         // construction du mot
+//         else
+//             word += *it;
+//     }
+//     // ajout du dernier mot
+//     result.push_back(word);
+//     return result;
+// }
 
 int main()
 {
