973940bc628e63b897156d2b4d835d5447b62730
smallboyc <maxencedup44@gmail.com>
2024-04-08 14:19:26 +0200

    fix : TD02 counting_sort

--- TD02/src/counting_sort/main.cpp
+++ TD02/src/counting_sort/main.cpp
@@ -14,18 +14,33 @@ int checkMaxNumber(std::vector<int> const &array)
 
 std::vector<int> sortArray(std::vector<int> const &array, int const &max)
 {
-    std::vector<int> iterationArray(max + 1);
+    // std::vector<int> iterationArray(max + 1);
+    // std::vector<int> final;
+    // for (int i{0}; i < iterationArray.size(); i++)
+    // {
+    //     for (int j{0}; j < array.size(); j++)
+    //     {
+    //         if (i == array[j])
+    //             iterationArray[i]++;
+    //     }
+    //     if (iterationArray[i] != 0)
+    //         for (int j{0}; j < iterationArray[i]; j++)
+    //             final.push_back(i);
+    // }
+    // return final;
+
+    std::vector<int> index_array(max + 1, 0);
     std::vector<int> final;
-    for (int i{0}; i < iterationArray.size(); i++)
+    for (const int element : array)
+        index_array[element]++;
+
+    int i{0};
+    while (i < index_array.size())
     {
-        for (int j{0}; j < array.size(); j++)
-        {
-            if (i == array[j])
-                iterationArray[i]++;
-        }
-        if (iterationArray[i] != 0)
-            for (int j{0}; j < iterationArray[i]; j++)
+        if (index_array[i] != 0)
+            for (int j{0}; j < index_array[i]; j++)
                 final.push_back(i);
+        i++;
     }
     return final;
 }
